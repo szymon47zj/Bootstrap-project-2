fc0e278c0c3c4f8e0dc44bd8305b99f9477b4572
szymonbalinski <szymon.balinski@gmail.com>
2018-08-27 12:45:29 +0200

    smooth scrolling added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+	$("a[href^='#']").on('click', function (e) {
+		var target = $($(this).attr('href'));
+
+		if (target.length) {
+			e.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 1000)
+		}
+	})
+})
